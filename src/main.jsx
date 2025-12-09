460c15d77459ba3b704334c928987ad782853f04
Evan King <evanking@eking@c.ringling.edu>
2025-12-08 23:20:53 -0500

    Updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <h1 className="text-3xl font-bold underline text-blue-600">
+      Test
+    </h1>
+  </React.StrictMode>
+);
